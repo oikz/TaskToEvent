6f36975e010638eb6708c2cc5decf6f4962ce747
oikz <chapmanjgm@gmail.com>
2021-11-26 21:11:36 +1300

    Find the specified Task list and Output all tasks in it

--- TaskToEvent.cs
+++ TaskToEvent.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Graph;
 
 namespace TaskToEvent {
     public class TaskToEvent {
-        private static readonly string appID = "855929bc-bbb8-475b-84ff-7a93c0b91019"; //Spooky appID
-        private static readonly string[] scopes = {"User.Read", "Tasks.Read", "Calendars.ReadWrite"};
-        
+        private const string AppId = "855929bc-bbb8-475b-84ff-7a93c0b91019"; //Spooky appID
+        private static readonly string[] Scopes = { "User.Read", "Tasks.Read", "Calendars.ReadWrite" };
+        private const string ListName = "Tasks";
+
+        /// <summary>
+        /// Initialise the application and authenticate the user
+        /// </summary>
+        /// <param name="args">Command line arguments (Unused)</param>
         public static async Task Main(string[] args) {
             // Initialize the auth provider with values from fields above
-            var authProvider = new DeviceCodeAuthProvider(appID, scopes);
-            
+            var authProvider = new DeviceCodeAuthProvider(AppId, Scopes);
+
             var graphClient = new GraphServiceClient(authProvider);
 
             //Create new folder for storing data if not already created
@@ -20,15 +27,64 @@ namespace TaskToEvent {
             await GetTasks(graphClient);
         }
 
+        /// <summary>
+        /// Get all of the tasks in the specified list
+        /// </summary>
+        /// <param name="graphClient">The GraphClient to send requests to</param>
         private static async Task GetTasks(GraphServiceClient graphClient) {
             //Get all tasks
             var user = await graphClient.Me.Request().GetAsync();
             Console.WriteLine(user.DisplayName);
+
+            // Find the user specified list
+            var list = await FindList(graphClient);
+            var tasks = new List<TodoTask>();
+
+            // Add all tasks to the list
+            var todoTasks = await graphClient.Me.Todo.Lists[list.Id].Tasks.Request().GetAsync();
+            tasks.AddRange(todoTasks.Where(todoTask =>
+                todoTask.IsReminderOn != null && todoTask.CompletedDateTime == null));
+
+
+            foreach (var todoTask in tasks) {
+                Console.WriteLine($"Task: {todoTask.Title}");
+            }
             
             var calendars = await graphClient.Me.Calendars.Request().GetAsync();
             foreach (var calendar in calendars) {
-                Console.WriteLine(calendar.Name);
+                Console.WriteLine($"Calendar: {calendar.Name}");
             }
+        }
+
+        /// <summary>
+        /// Find the Task List specified by the user
+        /// </summary>
+        /// <param name="graphClient">The GraphClient to send requests to</param>
+        /// <returns>The Task List</returns>
+        private static async Task<TodoTaskList> FindList(GraphServiceClient graphClient) {
+            var taskLists = await graphClient.Me.Todo.Lists.Request().GetAsync();
+            foreach (var taskList in taskLists) {
+                if (taskList.DisplayName == ListName) {
+                    return taskList;
+                }
+            }
+
+            //Retry looking for the task list up to 5 times
+            for (var i = 0; i < 5; i++) {
+                if (taskLists.NextPageRequest != null)
+                    taskLists = await taskLists.NextPageRequest.GetAsync();
+
+                foreach (var taskList in taskLists) {
+                    if (taskList.DisplayName == ListName) {
+                        return taskList;
+                    }
+                }
+            }
+
+            Console.WriteLine("Could not find list");
+            Environment.Exit(0);
+
+            return new TodoTaskList();
         }
     }
 }
